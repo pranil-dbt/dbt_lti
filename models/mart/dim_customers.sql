8cbd80eb49f530f2527aca6fd4f6238c7b38a105
pranil.parbalkar@lntinfotech.com <pranil.parbalkar@lntinfotech.com>
2022-04-26 01:45:07 +0000

    snap-1

---
+++ models/mart/dim_customers.sql
@@ -0,0 +1,44 @@
+-- Use CTEs for easy reading and debugging
+{{ config(materialized='table') }}
+
+with customer as (
+select
+*
+from {{ ref('stg_cust') }}
+),
+
+nation as (
+select
+*
+from
+{{ ref('stg_nation') }}
+),
+
+region as (
+select
+*
+from
+{{ ref('stg_region') }}
+),
+final as (
+select
+        customer.customer_id,
+        customer.name,
+        customer.address,
+        nation.nation_id as nation_id,
+        nation.name as nation,
+        region.region_id as region_id,
+        region.name as region,
+        customer.phone_number,
+        customer.account_balance,
+        customer.market_segment 
+        from customer
+        inner join nation
+            on customer.nation_id = nation.nation_id
+        inner join region
+            on nation.region_id = region.region_id
+
+)
+select * from final
+
+    
---
+++ models/stage/mk_mall_doc.md
@@ -0,0 +1,6 @@
+{% docs primary_key %}
+Primary Key for a table
+{% enddocs %}
+{% docs foreign_key %}
+Foreign Key for a table
+{% enddocs %}
